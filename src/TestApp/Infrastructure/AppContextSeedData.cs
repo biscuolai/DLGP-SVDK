c79eba1ad5f4c67499bbaae08d8331f2f9fcbaba
Ilson Biscuola <ilson_biscuola@yahoo.com>
2016-05-17 21:20:19 +0800

    Added LookupValue to Seeddata

--- src/TestApp/Infrastructure/AppContextSeedData.cs
+++ src/TestApp/Infrastructure/AppContextSeedData.cs
@@ -83,6 +83,37 @@ namespace DLGP_SVDK.Infrastructure
                     // save changes
                     _context.SaveChanges();
                 }
+
+                // Add LookupValue data
+                if (!_context.LookupValues.Any())
+                {
+                    List<LookupValue> LV = new List<LookupValue>()
+                    {
+                        new LookupValue() { Active = true, Name = LookupValueName.Category, Order = 0, Value = "Bug" },
+                        new LookupValue() { Active = true, Name = LookupValueName.Category, Order = 1, Value = "Enhancement" },
+                        new LookupValue() { Active = true, Name = LookupValueName.Category, Order = 2, Value = "Request For Information" },
+                        new LookupValue() { Active = true, Name = LookupValueName.ConfigurationItem, Order = 0, Value = "Service Desk" },
+                        new LookupValue() { Active = true, Name = LookupValueName.ConfigurationItem, Order = 1, Value = "VRAT" },
+                        new LookupValue() { Active = true, Name = LookupValueName.ContactType, Order = 0, Value = "Portal" },
+                        new LookupValue() { Active = true, Name = LookupValueName.ContactType, Order = 1, Value = "Email" },
+                        new LookupValue() { Active = true, Name = LookupValueName.ContactType, Order = 2, Value = "Phone" },
+                        new LookupValue() { Active = true, Name = LookupValueName.Priority, Order = 0, Value = "1 - Critical" },
+                        new LookupValue() { Active = true, Name = LookupValueName.Priority, Order = 1, Value = "2 - High" },
+                        new LookupValue() { Active = true, Name = LookupValueName.Priority, Order = 2, Value = "3 - Medium" },
+                        new LookupValue() { Active = true, Name = LookupValueName.Priority, Order = 3, Value = "4 - Low" },
+                        new LookupValue() { Active = true, Name = LookupValueName.TicketStatus, Order = 0, Value = "New" },
+                        new LookupValue() { Active = true, Name = LookupValueName.TicketStatus, Order = 1, Value = "Open" },
+                        new LookupValue() { Active = true, Name = LookupValueName.TicketStatus, Order = 2, Value = "Ready For Test" },
+                        new LookupValue() { Active = true, Name = LookupValueName.TicketStatus, Order = 3, Value = "Pending" },
+                        new LookupValue() { Active = true, Name = LookupValueName.TicketStatus, Order = 4, Value = "Cancelled" },
+                        new LookupValue() { Active = true, Name = LookupValueName.TicketStatus, Order = 5, Value = "Closed" }
+                    };
+
+                    _context.LookupValues.AddRange(LV);
+
+                    // save changes
+                    _context.SaveChanges();
+                }
             }
             catch (Exception ex)
             {
