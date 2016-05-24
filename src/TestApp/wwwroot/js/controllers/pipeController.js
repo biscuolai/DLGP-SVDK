7be0a4bf165d23b9f99837dcdb7e6954def9e36b
Ilson Biscuola <ilson_biscuola@yahoo.com>
2016-05-24 23:01:10 +0800

    filtering using views is working. To do My tickets

--- src/TestApp/wwwroot/js/controllers/pipeController.js
+++ src/TestApp/wwwroot/js/controllers/pipeController.js
@@ -13,12 +13,10 @@
                 { id: 0, name: 'All Tickets' },
                 { id: 1, name: 'My Tickets' },
                 { id: 2, name: 'All New Tickets' },
-                { id: 2, name: 'All Open Tickets' },
-                { id: 2, name: 'All Pending Tickets' },
-                { id: 2, name: 'All Closed Tickets' }
+                { id: 3, name: 'All Open Tickets' },
+                { id: 4, name: 'All Pending Tickets' },
+                { id: 5, name: 'All Closed Tickets' }
             ];
-
-            //this.status = { name : '' };
 
             this.displayed = [];
 
@@ -26,15 +24,9 @@
 
                 ctrl.isLoading = true;
 
-                var pagination = tableState.pagination;
+                ctrl.query = '';
 
-                // setting predicateObject as Ticket Status selected
-                //if (tableState.search.predicateObject === undefined) {
-                //    tableState.search.predicateObject = {};
-                //}
-                //if ((ctrl.status !== undefined) && (ctrl.status !== '') && (ctrl.status !== 'All')) {
-                //    tableState.search.predicateObject.status = ctrl.status;
-                //}
+                var pagination = tableState.pagination;
 
                 var start = pagination.start || 0;      // This is NOT the page number, but the index of item in the list that you want to use to display the table.
                 var number = pagination.number || 5;    // Number of entries showed per page.
@@ -46,31 +38,39 @@
                 });
             };
 
-            //this.setTicketStatus = function setTicketStatus(status) {
-            //    ctrl.status.name = status;
-            //}
+            this.queryResponse = "";
+            this.submitSearch = function (query) {
+                // place ajax http code here
+                this.queryResponse = query;
+            };
 
             this.showTicketList = function showTicketList(id) {
-
-                debugger;
 
                 switch(id)
                 {
                     // all tickets
                     case 0:
-                        ctrl.selectedPredicate = '';
-                        ctrl.selectedPredicateValue = '';
+                        ctrl.queryResponse = '';
                         break;
                     // my tickets
                     case 1:
-                        ctrl.selectedPredicate = '';
-                        ctrl.selectedPredicateValue = '';
+                        ctrl.queryResponse = '';
                         break;
-                    // all new tickets
+                        // all new tickets
                     case 2:
-                        ctrl.selectedPredicateValue = '';
-                        ctrl.selectedPredicate = 'status.name';
-                        ctrl.selectedPredicateValue = 'new';
+                        ctrl.queryResponse = 'new';
+                        break;
+                        // all open tickets
+                    case 3:
+                        ctrl.queryResponse = 'open';
+                        break;
+                        // all pending tickets
+                    case 4:
+                        ctrl.queryResponse = 'pending';
+                        break;
+                        // all closed tickets
+                    case 5:
+                        ctrl.queryResponse = 'closed';
                         break;
                     default:
                         //
@@ -79,35 +79,20 @@
             }
 
         }])
-        //.filter('datagridFilter', function ($filter) {
-        //    return function (input, predicate) {
-        //        return $filter('filter')(input, predicate, true);
-        //    }
-        //})
-        //.filter('unique', function () {
-        //    return function (arr, field) {
-        //        var o = {}, i, l = arr.length, r = [], property = '', subProperty = '';
-        //        if (field.indexOf('.') != -1) {
-        //            var res = field.split('.');
-        //            property = res[0];
-        //            subProperty = res[1];
-        //        }
-        //        for (i = 0; i < l; i++) {
-        //            if (property !== '') {
-        //                o[arr[i][property][subProperty]] = arr[i];
-        //            }
-        //            else {
-        //                o[arr[i][field]] = arr[i];
-        //            }
-        //        }
+    .directive('searchWatchModel', function () {
+        return {
+            require: '^stTable',
+            scope: {
+                searchWatchModel: '='
+            },
+            link: function (scope, ele, attr, ctrl) {
+                var table = ctrl;
 
-        //        // push all values to unique dropdown list
-        //        for (i in o) {
-        //            r.push(o[i]);
-        //        }
+                scope.$watch('searchWatchModel', function (val) {
+                    ctrl.search(val);
+                });
 
-        //        return r;
-        //    };
-    //});
-
+            }
+        };
+    });
 })();
