ed737513a692f3914b340600b5e4d9145810f240
WILESP <phil_wiles@dialog.com.au>
2016-05-25 13:16:54 +0800

    Fixed up Request datagrid with items. Added chart controls to Dashboard

--- src/TestApp/wwwroot/js/controllers/chartController.js
+++ src/TestApp/wwwroot/js/controllers/chartController.js
@@ -7,14 +7,37 @@
 
     function chartController($scope) {
 
-        $scope.title = 'chartController';
-
         $scope.labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-        $scope.series = ['Series A', 'Series B'];
         $scope.data = [
           [65, 59, 80, 81, 56, 55, 40],
           [28, 48, 40, 19, 86, 27, 90]
         ];
+        $scope.colours = [
+          { // grey
+              fillColor: 'rgba(148,159,177,0.2)',
+              strokeColor: 'rgba(148,159,177,1)',
+              pointColor: 'rgba(148,159,177,1)',
+              pointStrokeColor: '#fff',
+              pointHighlightFill: '#fff',
+              pointHighlightStroke: 'rgba(148,159,177,0.8)'
+          },
+          { // dark grey
+              fillColor: 'rgba(77,83,96,0.2)',
+              strokeColor: 'rgba(77,83,96,1)',
+              pointColor: 'rgba(77,83,96,1)',
+              pointStrokeColor: '#fff',
+              pointHighlightFill: '#fff',
+              pointHighlightStroke: 'rgba(77,83,96,1)'
+          }
+        ];
+        $scope.randomize = function () {
+            $scope.data = $scope.data.map(function (data) {
+                return data.map(function (y) {
+                    y = y + Math.random() * 10 - 5;
+                    return parseInt(y < 0 ? 0 : y > 100 ? 100 : y);
+                });
+            });
+        };
 
 
         //$scope.labels2 = ['Download Sales', 'Store Sales', 'Mail Sales', 'Telesales', 'Corporate Sales'];
