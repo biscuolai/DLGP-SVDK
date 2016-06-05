38264f33672be33e5d597a6184b3a9f0debf920c
Ilson Biscuola <ilson_biscuola@yahoo.com>
2016-06-06 01:38:45 +0800

    New ApplicationUser class

---
+++ src/Model/Domain/Entities/Identity/ApplicationUser.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace DLGP_SVDK.Model.Domain.Entities.Identity
+{
+    // Add profile data for application users by adding properties to the ApplicationUser class
+    public class ApplicationUser : IdentityUser
+    {
+        [Required(ErrorMessageResourceName = "FieldRequired")]
+        [StringLength(100, ErrorMessageResourceName = "FieldMaximumLength")]
+        [Display(Name = "DisplayName", ResourceType = typeof(String))]
+        public string DisplayName { get; set; }
+
+        //public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ServiceDeskUser> manager)
+        public async Task<IdentityResult> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            //var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            //userIdentity.AddClaim(new Claim(ClaimTypes.GivenName, DisplayName));
+
+            var userIdentity = await manager.AddClaimAsync(this, new Claim(ClaimTypes.GivenName, DisplayName));
+
+            return userIdentity;
+        }
+    }
+}
